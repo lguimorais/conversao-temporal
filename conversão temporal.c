2dfc9f92ff1df89ae5c9b4be73ca8e11a18c8f17
luis guilherme <tete42449@gmail.com>
2024-05-07 01:36:44 -0300

    inicio

---
+++ conversão temporal.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+
+float horas, minutos, segundos, tempo;
+int main()
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+
+}
